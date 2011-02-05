94840110e333309b42b4b13ed23ddb1fcc788f62
guilleiguaran <guilleiguaran@gmail.com>
2011-02-05 03:30:34 -0500

    Adding problems, and text solutions

--- 1/2.scm
+++ 1/2.scm
@@ -1,4 +1,6 @@
-(/ (+ 5 4
-    (- 2 (- 3 (+ 6 (/ 1 3)))))
- (* 3
-  (- 6 2) (- 2 7)))
+;; SICP Exercise 1.2
+;;
+;; Translate the following expression into prefix form (Figure on book)
+
+(/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 1 3)))))
+ (* 3 (- 6 2) (- 2 7)))
--- 1/3.scm
+++ 1/3.scm
@@ -1,12 +1,17 @@
-(define (square x)
- (* x x))
+;; SICP Exercise 1.3
+;;
+;; Define a procedure that takes three numbers as arguments and returns the sum of the squares of the two larger numbers.
 
-(define (greater x y)
- (if (> x y) x y))
-
-(define (lesser x y)
- (if (< x y) x y))
 
 (define (sum-of-greatest-squares x y z)
- (+ (square (greater x y))
-  (square (greater (lesser x y) z))))
+  (define (square x)
+   (* x x))
+  (define (sum-of-squares x y)
+   (+ (square x) (square y)))
+  (if (> x y)
+    (if (> y z) (sum-of-squares x y) (sum-of-squares x z))
+    (if (> x z) (sum-of-squares y x) (sum-of-squares y z))))
+
+
+; test
+(sum-of-greatest-squares 1 2 3)
