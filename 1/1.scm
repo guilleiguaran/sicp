ef61b5736a9c91a36dc36c2bf589e2f69d86b0c3
Guillermo Iguaran <guilleiguaran@gmail.com>
2011-08-09 10:47:20 -0500

    Answers for 1.1

--- 1/1.scm
+++ 1/1.scm
@@ -3,23 +3,23 @@
 ;; Below is a sequence of expressions. What is the result printed by the interpreter in response to each expression?
 ;; Assume that the sequence is to be evaluated in the order in which it is presented.
 
-10
-(+ 5 3 4)
-(- 9 1)
-(/ 6 2)
-(+ (* 2 4) (- 4 6))
-(define a 3)
-(define b (+ a 1))
-(+ a b (* a b))
-(= a b)
-(if (and (> b a) (< b (* a b)))
+10                                ; 10
+(+ 5 3 4)                         ; 12 
+(- 9 1)                           ; 8
+(/ 6 2)                           ; 3
+(+ (* 2 4) (- 4 6))               ; 6
+(define a 3)                      ; a
+(define b (+ a 1))                ; b
+(+ a b (* a b))                   ; 19
+(= a b)                           ; #f
+(if (and (> b a) (< b (* a b)))   ; 4
     b
     a)
-(cond ((= a 4) 6)
+(cond ((= a 4) 6)                 ; 16
       ((= b 4) (+ 6 7 a))
       (else 25))
-(+ 2 (if (> b a) b a))
-(* (cond ((> a b) a)
+(+ 2 (if (> b a) b a))            ; 6
+(* (cond ((> a b) a)              ; 16
          ((< a b) b)
          (else -1))
    (+ a 1))
